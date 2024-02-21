5d4a2426fdc7a75d35dbf8ec17fefacde7cad724
Victor Shcherbakov <victor@victor.fm>
2024-02-22 01:55:59 +0400

    Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Lato, Poppins } from "next/font/google";
 import "./globals.scss";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const lato = Lato({
   weight: ["100", "300", "400", "700", "900"],

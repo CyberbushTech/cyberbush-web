3a02cb8e2a28289353a30e72d6ad4f117e08cf87
Victor Shcherbakov <victor@victor.fm>
2024-02-23 00:18:35 +0400

    Google Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Lato, Poppins } from "next/font/google";
 import "./globals.scss";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const lato = Lato({
   weight: ["100", "300", "400", "700", "900"],
@@ -18,6 +19,8 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "Cyberbush",
+  description:
+    "AI-controlled aircrafts producer for air taxi, entertainment and delivery",
   icons: {
     icon: ["/icon.png"],
     apple: ["/icon-apple.png"],
@@ -36,6 +39,7 @@ export default function RootLayout({
         {children}
         <Analytics />
         <SpeedInsights />
+        <GoogleAnalytics gaId="G-71XESJX2YN" />
       </body>
     </html>
   );

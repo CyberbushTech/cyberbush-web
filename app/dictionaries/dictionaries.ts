9bbe901262150a824a3bf52faee59e406649ad58
Victor Shcherbakov <victor@victor.fm>
2025-02-21 00:29:50 +0300

    Locale fix

--- app/dictionaries/dictionaries.ts
+++ app/dictionaries/dictionaries.ts
@@ -5,12 +5,7 @@ const dictionaries = {
 
 const getDictionary = (locale: "en" | "ru") => dictionaries[locale].dict;
 const getCurrentLocale = () => {
-  const hostname =
-    process.env.VERCEL_PROJECT_PRODUCTION_URL ?? "cyberbush.tech";
-  if (hostname === "localhost") {
-    return "ru";
-  }
-  return hostname.endsWith(".ru") ? "ru" : "en";
+  return process.env.CYBER_LOCALE == "ru" ? "ru" : "en";
 };
 
 export const getLocalizations = () => getDictionary(getCurrentLocale());
